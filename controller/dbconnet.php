7de59b61eeacc830351a0479b8126a8310fe01d4
Ogbonna Vitalis <o.vitalis@genesystechhub.com>
2019-04-26 22:47:58 +0100

    setting up cleardb

--- controller/dbconnet.php
+++ controller/dbconnet.php
@@ -3,13 +3,23 @@ $servername = "localhost";
 $user = "root";
 $password = "";
 $dbname = "judith";
-// Create connection
-$conn = mysqli_connect($servername, $user, $password,$dbname);
+
+//connection for heroku
+    $servername_live = "us-cdbr-iron-east-02.cleardb.net";
+    $user_live = "baa9c44a654512";
+    $password_live = "c1daa00b";
+    $dbname_live = "heroku_026a3952fc42287";
+    // Create connection
+    
+    $conn = mysqli_connect($servername, $user, $password,$dbname);
+    //$conn = mysqli_connect($servername_live, $user_live, $password_live,$dbname_live);
+
+
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysql://baa9c44a654512:c1daa00b@us-cdbr-iron-east-02.cleardb.net/heroku_026a3952fc42287?reconnect=true
+//mysql://baa9c44a654512:c1daa00b@us-cdbr-iron-east-02.cleardb.net/heroku_026a3952fc42287?reconnect=true
 ?> 
